c342e696ff59fcd903d88f720049d4f175622f39
Costya <costya.y@qualisystems.com>
2024-02-01 14:02:19 -0600

    initial commit

---
+++ cloudshell/cp/cloudstack/actions/vm_details.py
@@ -0,0 +1,77 @@
+from logging import Logger
+
+from cloudshell.cp.core.request_actions.models import (
+    DeployedApp,
+    VmDetailsData,
+    VmDetailsNetworkInterface,
+    VmDetailsProperty,
+)
+
+from cloudshell.cp.cloudstack.entities.vm import CloudstackVirtualMachine
+from cloudshell.cp.cloudstack.models.resource_config import CloudstackResourceConfig
+from cloudshell.cp.cloudstack.services.cloudstack_api_service import (
+    CloudStackAPIService,
+)
+
+
+def get_vm(
+    resource_conf: CloudstackResourceConfig, deployed_app: DeployedApp, logger: Logger
+):
+    cloudstack_api = resource_conf.get_cloudstack_api()
+    return CloudstackVirtualMachine(cloudstack_api, deployed_app.vmdetails.uid, logger)
+
+
+def _sort_networks_key(mgmt_net_id: str):
+    def sort(net_iface: VmDetailsNetworkInterface) -> bool:
+        # iface with mgmt network would be first
+        return net_iface.networkId != mgmt_net_id
+
+    return sort
+
+
+def get_vm_details(
+    cloudstack_api: CloudStackAPIService,
+    mgmt_network_id: str,
+    vm: CloudstackVirtualMachine,
+    logger,
+):
+    logger.info(
+        f"Getting Cloudstack VirtualMachine details for "
+        f"CloudstackVirtualMachine: {vm.vm_name}"
+    )
+
+    vm_instance_data = [
+        # VmDetailsProperty(key='Instance Name', value=vm.vm_data['name'])
+        VmDetailsProperty(key="CPU", value=f"{vm.num_cpu} vCPU"),
+        VmDetailsProperty(key="Memory", value=vm.memory_size),
+        VmDetailsProperty(key="Guest OS", value=vm.guest_os),
+        VmDetailsProperty(key="ID", value=vm.vm_uuid),
+    ]
+
+    vm_network_data = []
+
+    for nic in vm.get_interfaces():
+        primary = False
+        if nic.network_id == mgmt_network_id:
+            primary = True
+        network_data = [
+            VmDetailsProperty(key="MAC Address", value=nic.mac_address),
+            VmDetailsProperty(key="IP", value=nic.ip_address),
+        ]
+        vlan_id = cloudstack_api.Network.find_by_id(nic.network_id).vlan_id
+
+        current_interface = VmDetailsNetworkInterface(
+            interfaceId=nic.mac_address,
+            networkId=vlan_id,
+            isPrimary=primary,
+            isPredefined=primary,
+            networkData=network_data,
+            privateIpAddress=nic.ip_address,
+            publicIpAddress=nic.public_ip_address,
+        )
+        vm_network_data.append(current_interface)
+    return VmDetailsData(
+        vmInstanceData=vm_instance_data,
+        vmNetworkData=sorted(vm_network_data, key=_sort_networks_key(mgmt_network_id)),
+        appName=vm.vm_name,
+    )
---
+++ cloudshell/cp/cloudstack/entities/network.py
@@ -0,0 +1,180 @@
+from __future__ import annotations
+
+from collections.abc import Generator
+from logging import Logger
+from typing import TYPE_CHECKING, ClassVar
+
+import attr
+
+from cloudshell.cp.cloudstack.entities.network_type import NetworkType
+from cloudshell.cp.cloudstack.exceptions import (
+    CreateNetworkError,
+    NetworkInUse,
+    NetworkNotFound,
+)
+from cloudshell.cp.cloudstack.models.connectivity_action_model import SubnetCidrData
+
+if TYPE_CHECKING:
+    from cloudshell.cp.cloudstack.api_client import CloudStackAPIService
+
+
+@attr.s(auto_attribs=True, str=False)
+class Network:
+    api: ClassVar[CloudStackAPIService]
+    _logger: ClassVar[Logger]
+
+    id_: str  # noqa: A003
+    name: str
+    network_type: NetworkType
+    vlan_id: int | None
+    is_external: bool
+
+    def __str__(self) -> str:
+        return f"Network '{self.name}'"
+
+    @classmethod
+    def from_dict(cls, net_dict: dict) -> Network:
+        network_type = NetworkType(net_dict.get("type"))
+        is_external = (
+            network_type.value == NetworkType.SHARED
+            or network_type.value == NetworkType.VLAN
+        )
+        return cls(
+            net_dict["id"],
+            net_dict["displaytext"],
+            network_type=network_type,
+            vlan_id=net_dict.get("vlan"),
+            is_external=is_external,
+        )
+
+    @classmethod
+    def find_by_vlan_id(cls, vlan_id: int) -> Network:
+        networks = cls._list_networks({"vlan": vlan_id})
+        try:
+            net_dict = networks[0]
+        except (TypeError, IndexError):
+            raise NetworkNotFound(vlan_id=vlan_id) from None
+        return cls.from_dict(net_dict)
+
+    @classmethod
+    def find_by_id(cls, id_: str) -> Network:
+        networks = cls._list_networks({"id": id_})
+        try:
+            network = networks[0]
+        except (TypeError, IndexError):
+            raise NetworkNotFound(id_=id_) from None
+        return network
+
+    @classmethod
+    def find_by_name(cls, name: str, raise_error=True) -> Network:
+        networks = cls._list_networks({"name": name})
+        if networks is None:
+            raise NetworkNotFound(name=name)
+        for network in networks:
+            if network.name == name:
+                return network
+        if raise_error:
+            raise NetworkNotFound(name=name)
+
+    @classmethod
+    def find_by_id_or_name(cls, data: str, raise_error=True) -> Network:
+        networks = cls._list_networks()
+        if networks is None:
+            raise NetworkNotFound(name=data)
+        for network in networks:
+            if network.name == data:
+                return network
+            if network.id_ == data:
+                return network
+        if raise_error:
+            raise NetworkNotFound(name=data)
+
+    @classmethod  # noqa: A003
+    def all(cls) -> Generator[Network, None, None]:  # noqa: A003
+        cls._logger.debug("Get all networks")
+        return cls._list_networks()
+
+    @classmethod
+    def _list_networks(cls, filter: dict = None) -> list[Network, None, None]:
+        inputs = {"command": "listNetworks"}
+        if filter:
+            inputs.update(filter)
+        # add filter by tags
+        response = cls.api.send_request(inputs)
+        networks = response.json()["listnetworksresponse"]["network"]
+
+        if response.status_code != 200 and response.status_code != 201:
+            raise Exception(
+                f"Unable to verify API response. " f"{response.status_code}."
+            )
+        # for net_dict in offerings:
+        return [cls.from_dict(net_dict) for net_dict in networks]
+
+    @classmethod
+    def create(
+        cls,
+        name: str,
+        zone_id: str,
+        networking_offering: str,
+        *,
+        vlan_id: str | None = None,
+        subnet_cidr_data: SubnetCidrData | None = None,
+        qnq: bool = False,
+        physical_iface_name: str | None = None,
+    ) -> Network:
+        inputs = {
+            "command": "createNetwork",
+            "name": name,
+            "zoneid": zone_id,
+            "networkofferingid": networking_offering,
+            "vlan": vlan_id,
+            "bypassvlanoverlapcheck": "True",
+        }
+        if subnet_cidr_data:
+            inputs.update(
+                {
+                    "gateway": str(subnet_cidr_data.gateway),
+                    "netmask": str(subnet_cidr_data.cidr.netmask),
+                    "startip": str(subnet_cidr_data.allocation_pool[0]),
+                    "endip": str(subnet_cidr_data.allocation_pool[1]),
+                }
+            )
+        cls._logger.debug(f"Creating a network {name}")
+        response = cls.api.send_request(inputs)
+        if response.status_code != 200 and response.status_code != 201:
+            raise CreateNetworkError(
+                name=name,
+                vlan_id=vlan_id,
+                error_message=response.json()
+                .get("createnetworkresponse", {})
+                .get("errortext"),
+            )
+        response_json = response.json()
+        id_ = (
+            response_json.get("createnetworkresponse", {}).get("network", {}).get("id")
+        )
+        job_id = response_json.get("createnetworkresponse", {}).get("jobid")
+        status = cls.api.wait_for_job(job_id)
+        if status != 1:
+            raise CreateNetworkError(
+                name=name,
+                vlan_id=vlan_id,
+                error_message=f"Network creation process complete with "
+                f"status: {status}",
+            )
+        return cls.find_by_id(id_)
+
+    def remove(self, raise_in_use: bool = True) -> None:
+        self._logger.debug(f"Removing {self}")
+        inputs = {"command": "deleteNetwork", "id": self.id_, "forced": "True"}
+        response = self.api.send_request(inputs)
+        if raise_in_use and (
+            response.status_code != 200 and response.status_code != 201
+        ):
+            raise NetworkInUse(self)
+        job_id = response.json().get("deletenetworkresponse", {}).get("jobid")
+        status = self.api.wait_for_job(job_id)
+        if status != 1:
+            self._logger.warning(
+                f"Network deletion process failed with status: {status}"
+            )
